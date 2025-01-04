1b1e8143204e68d416c25400797b9b4da24758e5
mayraisa3000 <mayraisa3000@gmail.com>
2025-01-04 14:07:59 -0600

    final

---
+++ Deaclavariables.java
@@ -0,0 +1,26 @@
+package variables;
+
+import java.lang.invoke.VarHandle;
+
+public class CasteoVariables {
+    public static void main(String[] args){
+       String LetraNum = "23";
+       int NumeroLetra = 78;
+       double NumeroLetrau = 89;
+       double NumeroLetrad = 7.9;
+       int NumeroEnter = 14;
+       int NumberONE = Integer.parseInt(LetraNum);
+       double NumberDou = Double.parseDouble(LetraNum);
+       String LetraStr = String.valueOf(NumeroLetra);
+       String LetraDou = String.valueOf(NumeroLetrau);
+       int NumberEnt = (int) NumeroLetrad;
+       Double StriBole = (double) NumeroEnter;
+       String VerdaderoAtex = "true";
+       boolean VerdadUno = true;
+       
+
+
+
+;
+    }
+}
