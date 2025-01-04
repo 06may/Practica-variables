92f76dc06e1255d780d358849663e8df6dffa047
mayraisa3000 <mayraisa3000@gmail.com>
2025-01-04 14:12:31 -0600

    codigo

---
+++ CorrigeVariables.java
@@ -0,0 +1,29 @@
+package variables;
+
+public class CorrigeVariables {
+    public static  void main(String[] args) {
+        int a = 9;
+        String b = "B";
+        String c = "colombia";
+        char d = 'A';
+        float e = 832248.2f;
+        int numero;
+        String nombre = "persona";
+        int edad;
+        int f = 9;
+        int edadd = 25;
+        int x = 45;
+        int y = 14;
+        String g = "k";
+        Double pesoReal = Double.parseDouble("65");
+        System.out.println("var1: " +9);
+        System.out.println("var2: " +b);
+        System.out.println("var3: " +c);
+        System.out.println("var4: " +d);
+        System.out.println("var5: " +e);
+        System.out.println("var6: " +nombre);
+
+
+
+    }
+}
